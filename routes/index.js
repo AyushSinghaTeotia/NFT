ae8fb932ec305e5b00d2444193911277baef34e3
rajkumaryadavlive <rajkumar.live.mp@gmail.com>
2021-09-10 19:03:29 +0530

    user login

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,24 @@ router.get('/',HomeController.index);
 router.get('/explore',HomeController.explore);
 router.get('/search',HomeController.exploreContent);
 router.get('/defi', function(req, res, next) {
-  res.render('defi',{ layout: 'layouts/front/layout' });
+  res.render('defi',{ layout: 'layouts/front/layout',name: req.session.re_usr_name});
 });
 
 router.get('/model',HomeController.model);
 
 router.get('/contact', function(req, res, next) {
-  res.render('contactus',{ layout: 'layouts/front/layout' });
+  res.render('contactus',{ layout: 'layouts/front/layout',name: req.session.re_usr_name });
 });
 router.get('/subscriptions',function(req,res){
-  res.render('subscription',{layout:'layouts/front/layout'});
+  res.render('subscription',{layout:'layouts/front/layout',name: req.session.re_usr_name});
 });
 
 router.get('/subscribed',function(req,res){
-   res.render('subscribed',{layout:'layouts/front/layout'});
+   res.render('subscribed',{layout:'layouts/front/layout',name: req.session.re_usr_name});
 });
 
 router.get('/buy-plan',function(req,res){
-  res.render('buy-plan',{layout:'layouts/front/layout'});
+  res.render('buy-plan',{layout:'layouts/front/layout',name: req.session.re_usr_name});
 });
 router.get('/item-details',HomeController.contentDetail);
 
